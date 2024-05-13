1c71be9e248eb147bbc7e655f087e841fe1e39a4
Roman Walch <walch@taceo.io>
2024-05-13 14:38:35 +0200

    network

--- mpc-core/src/protocols/gsz.rs
+++ mpc-core/src/protocols/gsz.rs
@@ -1,11 +1,22 @@
+use self::network::GSZNetwork;
 use ark_ff::PrimeField;
+use eyre::Report;
 use std::marker::PhantomData;
 
 pub mod fieldshare;
+pub mod network;
 pub mod pointshare;
 
-pub struct GSZProtocol<F> {
+pub struct GSZProtocol<F: PrimeField, N: GSZNetwork> {
+    network: N,
     field: PhantomData<F>,
 }
 
-impl<F: PrimeField> GSZProtocol<F> {}
+impl<F: PrimeField, N: GSZNetwork> GSZProtocol<F, N> {
+    pub fn new(network: N) -> Result<Self, Report> {
+        Ok(Self {
+            network,
+            field: PhantomData,
+        })
+    }
+}
