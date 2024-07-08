0cd8f18a72f5e4e739ee1b4d87264ffffdfe71f6
Roman Walch <walch@taceo.io>
2024-07-08 13:46:56 +0200

    work on mpc docu

--- mpc-core/src/traits.rs
+++ mpc-core/src/traits.rs
@@ -104,33 +104,72 @@ pub trait PrimeFieldMpcProtocol<F: PrimeField> {
     fn print(&self, to_print: &Self::FieldShareVec);
 }
 
+/// A trait representing the MPC operations required for extending the secret-shared Circom witness in MPC. The operations are generic over public and private (i.e., secret-shared) inputs.
 pub trait CircomWitnessExtensionProtocol<F: PrimeField>: PrimeFieldMpcProtocol<F> {
+    /// A type representing the values encountered during Circom compilation. It should at least containt public field elements and shared values.
     type VmType: Clone + Default + fmt::Debug + fmt::Display + From<Self::FieldShare> + From<F>;
+
+    /// Add two VM-types: c = a + b.
     fn vm_add(&mut self, a: Self::VmType, b: Self::VmType) -> Self::VmType;
+
+    /// Subtract the VM-type b from the VM-type a: c = a - b.
     fn vm_sub(&mut self, a: Self::VmType, b: Self::VmType) -> Self::VmType;
+
+    /// Multiply two VM-types: c = a * b.
     fn vm_mul(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Divide the VM-type a by the VM-type b: c = a / b. In finite fields, this is equivalent to multiplying a by the inverse of b.
     fn vm_div(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Integer division of the VM-type a by the VM-type b: c = a \ b.
     fn vm_int_div(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
 
+    /// Compute c = a ^ b, where a and b ar VM-types.
     fn vm_pow(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the modulo of the VM-type a by the VM-type b: c = a % b.
     fn vm_mod(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the square root of the VM-type a: c = sqrt(a).
     fn vm_sqrt(&mut self, a: Self::VmType) -> Result<Self::VmType>;
 
+    /// Compute the negation of the VM-type a: c = -a.
     fn vm_neg(&mut self, a: Self::VmType) -> Self::VmType;
 
+    /// Compute the less than operation of two VM-types: a < b. Outputs 1 if a < b, 0 otherwise.
     fn vm_lt(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the less or equal than operation of two VM-types: a <= b. Outputs 1 if a <= b, 0 otherwise.
     fn vm_le(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the greater than operation of two VM-types: a > b. Outputs 1 if a > b, 0 otherwise.
     fn vm_gt(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the greater or equal than operation of two VM-types: a >= b. Outputs 1 if a >= b, 0 otherwise.
     fn vm_ge(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the equality of two VM-types: a == b. Outputs 1 if a == b, 0 otherwise.
     fn vm_eq(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Compute the inequality of two VM-types: a != b. Outputs 1 if a != b, 0 otherwise.
     fn vm_neq(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
 
+    /// Computes the bitwise shift right of the VM-type a by the VM-type b: c = a >> b.
     fn vm_shift_r(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Computes the bitwise shift left of the VM-type a by the VM-type b: c = a << b.
     fn vm_shift_l(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
 
+    /// Computes the boolean NOT of the VM-type a, i.e. 1 - a. The value a is expected to either be 0 or 1.
     fn vm_bool_not(&mut self, a: Self::VmType) -> Result<Self::VmType>;
+
+    /// Computes the boolean AND of the VM-types a and b: c = a && b. The values a and b are expected to be either 0 or 1.
     fn vm_bool_and(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Computes the boolean OR of the VM-types a and b: c = a || b. The values a and b are expected to be either 0 or 1.
     fn vm_bool_or(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Computes a CMUX: If cond is 1, returns truthy, otherwise returns falsy.
     fn vm_cmux(
         &mut self,
         cond: Self::VmType,
@@ -138,20 +177,34 @@ pub trait CircomWitnessExtensionProtocol<F: PrimeField>: PrimeFieldMpcProtocol<F
         falsy: Self::VmType,
     ) -> Result<Self::VmType>;
 
+    /// Computes the bitwise XOR of the VM-types a and b: c = a ^ b.
     fn vm_bit_xor(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Computes the bitwise OR of the VM-types a and b: c = a | b.
     fn vm_bit_or(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
+
+    /// Computes the bitwise AND of the VM-types a and b: c = a & b.
     fn vm_bit_and(&mut self, a: Self::VmType, b: Self::VmType) -> Result<Self::VmType>;
 
+    /// Outputs whether a is zero (true) or not (false). This values is output in plain! Thus, if a is secret shared, the result is opened.
     fn is_zero(&mut self, a: Self::VmType, allow_secret_inputs: bool) -> Result<bool>;
+
+    /// Returns whether the VM-type represents a shared value (true) or a public one (false).
     fn is_shared(&mut self, a: &Self::VmType) -> Result<bool>;
 
+    /// Transforms a public field element into a usize if possible.
     fn vm_to_index(&mut self, a: Self::VmType) -> Result<usize>;
+
+    /// Opens the VM-type a. If a is secret shared, it gets reconstructed.
     fn vm_open(&mut self, a: Self::VmType) -> Result<F>;
 
+    /// Transforms a VM-type into a secret-shared value.
     fn vm_to_share(&self, a: Self::VmType) -> Self::FieldShare;
 
+    /// Returns F::one() as a VM-type.
     fn public_one(&self) -> Self::VmType;
-    //if default doesn't return the zero element, overwrite this function
+
+    /// Returns F::zero() as a VM-type. The default implementation uses the `Default` trait. If `Default` does not return 0, this function has to be overwritten.
     fn public_zero(&self) -> Self::VmType {
         Self::VmType::default()
     }
