91ad1052e29d10730c3588a959efdc201195151d
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-07-09 16:51:44 +0200

    docs: basic documentation for the collaborative-circom crate

--- collaborative-circom/src/file_utils.rs
+++ collaborative-circom/src/file_utils.rs
@@ -1,19 +1,26 @@
 use std::path::{Path, PathBuf};
 
+/// An error type for file utility functions.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    /// The file was not found.
     #[error("File not found: {0}")]
     FileNotFound(PathBuf),
+    /// The directory was not found.
     #[error("Dir not found: {0}")]
     DirNotFound(PathBuf),
+    /// The path was expected to be a directory, but it is a file.
     #[error("Expected {0} to be a directory, but it is a file.")]
     ExpectedDir(PathBuf),
+    /// The path was expected to be a file, but it is a directory.
     #[error("Expected {0} to be a file, but it is a directory.")]
     ExpectedFile(PathBuf),
+    /// An I/O error occurred.
     #[error(transparent)]
     IoError(#[from] std::io::Error),
 }
 
+/// Check if a file exists at the given path, and is actually a file.
 pub fn check_file_exists(file_path: &Path) -> Result<(), Error> {
     if !file_path.exists() {
         return Err(Error::FileNotFound(file_path.to_path_buf()));
@@ -24,6 +31,7 @@ pub fn check_file_exists(file_path: &Path) -> Result<(), Error> {
     Ok(())
 }
 
+/// Check if a directory exists at the given path, and is actually a directory.
 pub fn check_dir_exists(dir_path: &Path) -> Result<(), Error> {
     if !dir_path.exists() {
         return Err(Error::DirNotFound(dir_path.to_path_buf()));
--- collaborative-circom/src/lib.rs
+++ collaborative-circom/src/lib.rs
@@ -1,3 +1,5 @@
+#![warn(missing_docs)]
+//! This crate provides a binary and associated helper library for running collaborative SNARK proofs.
 use std::{io::Read, path::PathBuf};
 
 use ark_bn254::Bn254;
@@ -10,10 +12,14 @@ use color_eyre::eyre::Context;
 use mpc_core::protocols::rep3::{network::Rep3MpcNet, Rep3Protocol};
 use mpc_net::config::NetworkConfig;
 
+/// A module for file utility functions.
 pub mod file_utils;
 
+/// An enum representing the MPC protocol to use.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum MPCProtocol {
+    /// A protocol based on the Replicated Secret Sharing Scheme for 3 parties.
+    /// For more information see <https://eprint.iacr.org/2018/403.pdf>.
     REP3,
 }
 
@@ -37,18 +43,26 @@ impl std::fmt::Display for MPCProtocol {
     }
 }
 
+/// Try to parse a [SharedWitness] from a [Read]er.
 pub fn parse_witness_share<R: Read>(
     reader: R,
 ) -> color_eyre::Result<SharedWitness<Rep3Protocol<ark_bn254::Fr, Rep3MpcNet>, Bn254>> {
     bincode::deserialize_from(reader).context("trying to parse witness share file")
 }
 
+/// Try to parse a [SharedInput] from a [Read]er.
 pub fn parse_shared_input<R: Read>(
     reader: R,
 ) -> color_eyre::Result<SharedInput<Rep3Protocol<ark_bn254::Fr, Rep3MpcNet>, Bn254>> {
     bincode::deserialize_from(reader).context("trying to parse input share file")
 }
 
+/// Invoke the MPC witness generation process. It will return a [SharedWitness] if successful.
+/// It executes several steps:
+/// 1. Parse the circuit file.
+/// 2. Compile the circuit to MPC VM bytecode.
+/// 3. Set up a network connection to the MPC network.
+/// 4. Execute the bytecode on the MPC VM to generate the witness.
 pub fn generate_witness(
     circuit: String,
     link_library: Vec<String>,
@@ -83,6 +97,11 @@ pub fn generate_witness(
     Ok(result_witness_share)
 }
 
+/// Invoke the MPC proof generation process. It will return a [Proof] if successful.
+/// It executes several steps:
+/// 1. Construct a [Rep3Protocol] from the network configuration.
+/// 2. Construct a [CollaborativeGroth16] prover from the protocol.
+/// 3. Execute the proof in MPC
 pub fn prove_with_matrices(
     witness_share: SharedWitness<Rep3Protocol<ark_bn254::Fr, Rep3MpcNet>, Bn254>,
     config: NetworkConfig,
