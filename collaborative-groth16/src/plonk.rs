07a0e8d42d9c52566c4ea018fa1cb4a65d84dd36
Roman Walch <walch@taceo.io>
2024-08-02 14:26:56 +0200

    wip

--- collaborative-groth16/src/plonk.rs
+++ collaborative-groth16/src/plonk.rs
@@ -7,6 +7,7 @@ use ark_ff::PrimeField;
 use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
 use ark_relations::r1cs::SynthesisError;
 use ark_serialize::CanonicalSerialize;
+use circom_types::groth16::public_input;
 use circom_types::plonk::ZKey;
 use circom_types::traits::CircomArkworksPairingBridge;
 use circom_types::traits::CircomArkworksPrimeFieldBridge;
@@ -20,6 +21,9 @@ use std::io::Cursor;
 use std::marker::PhantomData;
 
 type Keccak256Transcript<P> = Transcript<Keccak256, P>;
+type FieldShare<T, P> = <T as PrimeFieldMpcProtocol<<P as Pairing>::ScalarField>>::FieldShare;
+type FieldShareVec<T, P> = <T as PrimeFieldMpcProtocol<<P as Pairing>::ScalarField>>::FieldShareVec;
+type PointShare<T, C> = <T as EcMpcProtocol<C>>::PointShare;
 
 struct Transcript<D, P>
 where
@@ -83,10 +87,11 @@ where
         println!("{bytes:?}");
         P::ScalarField::from_reader_unchecked_for_zkey(Cursor::new(bytes)).unwrap()
     }
+
+    fn reset(&mut self) {
+        self.digest.reset();
+    }
 }
-type FieldShare<T, P> = <T as PrimeFieldMpcProtocol<<P as Pairing>::ScalarField>>::FieldShare;
-type FieldShareVec<T, P> = <T as PrimeFieldMpcProtocol<<P as Pairing>::ScalarField>>::FieldShareVec;
-type PointShare<T, C> = <T as EcMpcProtocol<C>>::PointShare;
 
 struct Proof<P: Pairing> {
     commit_a: P::G1,
@@ -198,13 +203,27 @@ where
         res
     }
 
+    // TODO check if this is correct
+    fn get_witness(
+        &mut self,
+        private_witness: &SharedWitness<T, P>,
+        zkey: &ZKey<P>,
+        index: usize,
+    ) -> FieldShare<T, P> {
+        if index < zkey.n_public {
+            self.driver
+                .promote_to_trivial_share(private_witness.public_inputs[index])
+        } else {
+            T::index_sharevec(&private_witness.witness, index - zkey.n_public)
+        }
+    }
+
     fn compute_wire_polynomials(
         &mut self,
         challenges: &Challenges<T, P>,
         zkey: &ZKey<P>,
-        private_witness: SharedWitness<T, P>,
+        private_witness: &SharedWitness<T, P>,
     ) -> Result<WirePolyOutput<T, P>> {
-        let n8 = (P::ScalarField::MODULUS_BIT_SIZE + 7) / 8;
         let num_constraints = zkey.n_constraints;
 
         let mut buffer_a = Vec::with_capacity(num_constraints);
@@ -212,9 +231,9 @@ where
         let mut buffer_c = Vec::with_capacity(num_constraints);
 
         for i in 0..num_constraints {
-            buffer_a.push(T::index_sharevec(&private_witness.witness, zkey.map_a[i]));
-            buffer_b.push(T::index_sharevec(&private_witness.witness, zkey.map_b[i]));
-            buffer_c.push(T::index_sharevec(&private_witness.witness, zkey.map_c[i]));
+            buffer_a.push(self.get_witness(&private_witness, zkey, zkey.map_a[i]));
+            buffer_b.push(self.get_witness(&private_witness, zkey, zkey.map_b[i]));
+            buffer_c.push(self.get_witness(&private_witness, zkey, zkey.map_c[i]));
         }
 
         // TODO batch to montgomery in MPC?
@@ -258,14 +277,12 @@ where
         })
     }
 
-    fn compute_t(&mut self) {}
-
     fn round1(
         &mut self,
         challenges: &mut Challenges<T, P>,
         proof: &mut Proof<P>,
         zkey: &ZKey<P>,
-        private_witness: SharedWitness<T, P>,
+        private_witness: &SharedWitness<T, P>,
     ) -> Result<WirePolyOutput<T, P>> {
         // STEP 1.1 - Generate random blinding scalars (b0, ..., b10) \in F_p
         challenges.random_b(&mut self.driver)?;
@@ -289,12 +306,66 @@ where
         Ok(outp)
     }
 
-    fn round3(&mut self) -> Result<()> {
+    fn comput_z(&mut self) -> Result<FieldShareVec<T, P>> {
+        todo!()
+    }
+
+    fn round2(
+        &mut self,
+        transcript: &mut Keccak256Transcript<P>,
+        challenges: &mut Challenges<T, P>,
+        proof: &mut Proof<P>,
+        zkey: &ZKey<P>,
+        private_witness: &SharedWitness<T, P>,
+    ) -> Result<()>
+    where
+        P: Pairing + CircomArkworksPairingBridge,
+        P::BaseField: CircomArkworksPrimeFieldBridge,
+        P::ScalarField: CircomArkworksPrimeFieldBridge,
+    {
+        // STEP 2.1 - Compute permutation challenge beta and gamma \in F_p
+
+        // Compute permutation challenge beta
+        transcript.reset();
+        transcript.add_poly_commitment(zkey.verifying_key.qm);
+        transcript.add_poly_commitment(zkey.verifying_key.ql);
+        transcript.add_poly_commitment(zkey.verifying_key.qr);
+        transcript.add_poly_commitment(zkey.verifying_key.qo);
+        transcript.add_poly_commitment(zkey.verifying_key.qc);
+        transcript.add_poly_commitment(zkey.verifying_key.s1);
+        transcript.add_poly_commitment(zkey.verifying_key.s2);
+        transcript.add_poly_commitment(zkey.verifying_key.s3);
+
+        for val in private_witness.public_inputs.iter().cloned() {
+            transcript.add_scalar(val);
+        }
+
+        transcript.add_poly_commitment(proof.commit_a.into());
+        transcript.add_poly_commitment(proof.commit_b.into());
+        transcript.add_poly_commitment(proof.commit_c.into());
+
+        challenges.beta = transcript.get_challenge();
+
+        // Compute permutation challenge gamma
+        transcript.reset();
+        transcript.add_scalar(challenges.beta);
+        challenges.gamma = transcript.get_challenge();
+
+        // STEP 2.2 - Compute permutation polynomial z(X)
+        let poly_z = self.compute_z()?;
+
+        // STEP 2.3 - Compute permutation [z]_1
+        let commit_z =
+            MSMProvider::<P::G1>::msm_public_points(&mut self.driver, &zkey.p_tau, &poly_z);
+
+        proof.commit_z = self.driver.open_point(&commit_z)?;
+
         Ok(())
     }
 
-    fn round2(&mut self) -> Result<()> {
+    fn round3(&mut self) -> Result<()> {
         todo!();
+        Ok(())
     }
 }
 
@@ -361,7 +432,7 @@ mod tests {
         );
         let mut buf = vec![];
         let test = transcript.get_challenge();
-        test.serialize_uncompressed(&mut buf);
+        test.serialize_uncompressed(&mut buf).unwrap();
         println!("{:?}", buf);
         println!("{}", test);
     }
