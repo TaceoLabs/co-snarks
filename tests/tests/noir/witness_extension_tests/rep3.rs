e2b62c5ae27b968b30c64a7c78b18a8f6acd678e
Franco Nieddu <nieddu@taceo.io>
2024-08-23 10:08:04 +0200

    test: added more noir tests

--- tests/tests/noir/witness_extension_tests/rep3.rs
+++ tests/tests/noir/witness_extension_tests/rep3.rs
@@ -9,3 +9,5 @@ use tests::rep3_network::Rep3TestNetwork;
 use super::add_rep3_acvm_test;
 add_rep3_acvm_test!("addition_multiplication");
 add_rep3_acvm_test!("poseidon");
+add_rep3_acvm_test!("poseidon2");
+add_rep3_acvm_test!("poseidon_stdlib");
