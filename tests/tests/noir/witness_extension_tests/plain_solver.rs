b8967ec3e789b1e35f89cf978943a96bf899bc0c
Franco Nieddu <nieddu@taceo.io>
2024-08-23 13:29:25 +0200

    test: moved the KAT from target folder to kat folder (now finally in git)

--- tests/tests/noir/witness_extension_tests/plain_solver.rs
+++ tests/tests/noir/witness_extension_tests/plain_solver.rs
@@ -4,7 +4,11 @@ use noirc_artifacts::program::ProgramArtifact;
 
 use super::add_plain_acvm_test;
 
+// bassic assert zero tests
 add_plain_acvm_test!("addition_multiplication");
 add_plain_acvm_test!("poseidon");
 add_plain_acvm_test!("poseidon2");
 add_plain_acvm_test!("poseidon_stdlib");
+
+// memory tests
+add_plain_acvm_test!("slice");
