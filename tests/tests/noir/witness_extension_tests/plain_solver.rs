9b3da88a5b8e3ae5250a1bae7e92920c79ac4831
Franco Nieddu <nieddu@taceo.io>
2024-08-23 10:08:04 +0200

    test: added more noir tests

--- tests/tests/noir/witness_extension_tests/plain_solver.rs
+++ tests/tests/noir/witness_extension_tests/plain_solver.rs
@@ -6,3 +6,5 @@ use super::add_plain_acvm_test;
 
 add_plain_acvm_test!("addition_multiplication");
 add_plain_acvm_test!("poseidon");
+add_plain_acvm_test!("poseidon2");
+add_plain_acvm_test!("poseidon_stdlib");
