d91986e064a9ada2b02e794d2508484dedb9dc1c
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-08-29 11:59:42 +0200

    test: use crate-root relative paths for files in bench

--- co-circom/benches/zkey_parse.rs
+++ co-circom/benches/zkey_parse.rs
@@ -1,21 +1,26 @@
 use ark_bn254::Bn254;
 use criterion::{criterion_group, criterion_main, Criterion};
-use std::fs::File;
+use std::path::PathBuf;
 
 fn groth16_zkey_parse(c: &mut Criterion) {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("../test_vectors/Groth16/bn254/multiplier2/circuit.zkey");
+    let zkey = std::fs::read(d).unwrap();
+
     c.bench_function("groth16 zkey parse", |b| {
         b.iter(|| {
-            let zkey = File::open("../test_vectors/bn254/multiplier2/multiplier2.zkey").unwrap();
-            circom_types::groth16::ZKey::<Bn254>::from_reader(zkey).unwrap();
+            circom_types::groth16::ZKey::<Bn254>::from_reader(&zkey[..]).unwrap();
         })
     });
 }
 
 fn plonk_zkey_parse(c: &mut Criterion) {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("../test_vectors/Plonk/bn254/multiplier2/circuit.zkey");
+    let zkey = std::fs::read(d).unwrap();
     c.bench_function("plonk zkey parse", |b| {
         b.iter(|| {
-            let zkey = File::open("../test_vectors/Plonk/bn254/multiplier2.zkey").unwrap();
-            circom_types::plonk::ZKey::<Bn254>::from_reader(zkey).unwrap();
+            circom_types::plonk::ZKey::<Bn254>::from_reader(&zkey[..]).unwrap();
         })
     });
 }
