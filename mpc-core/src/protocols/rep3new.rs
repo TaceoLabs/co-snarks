a6ee07d72b27bf0c40b5214c392166a627aee9b6
Franco Nieddu <nieddu@taceo.io>
2024-09-03 14:30:40 +0200

    feat: added new rep3 impl

---
+++ mpc-core/src/protocols/rep3new.rs
@@ -0,0 +1,8 @@
+mod arithmetic;
+mod binary;
+
+pub use arithmetic::types::Rep3PrimeFieldShare;
+pub use arithmetic::types::Rep3PrimeFieldShareVec;
+pub use arithmetic::Arithmetic;
+
+pub use binary::types::Rep3BigUintShare;
---
+++ mpc-core/src/protocols/rep3new/binary/types.rs
@@ -0,0 +1,27 @@
+use num_bigint::BigUint;
+
+/// This type represents a packed vector of replicated shared bits. Each additively shared vector is represented as [BigUint]. Thus, this type contains two [BigUint]s.
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+pub struct Rep3BigUintShare {
+    pub(crate) a: BigUint,
+    pub(crate) b: BigUint,
+}
+
+impl Rep3BigUintShare {
+    /// Constructs the type from two additive shares.
+    pub fn new(a: BigUint, b: BigUint) -> Self {
+        Self { a, b }
+    }
+
+    pub(super) fn zero_share() -> Self {
+        Self {
+            a: BigUint::ZERO,
+            b: BigUint::ZERO,
+        }
+    }
+
+    /// Unwraps the type into two additive shares.
+    pub fn ab(self) -> (BigUint, BigUint) {
+        (self.a, self.b)
+    }
+}
