aa0b135caf04aa9336875416f263a848f93796bd
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-09-27 16:03:03 +0200

    feat: Add co-zeromorph to co-ultrahonk (#195)

--- co-noir/co-ultrahonk/src/co_decider/co_zeromorph/mod.rs
+++ co-noir/co-ultrahonk/src/co_decider/co_zeromorph/mod.rs
@@ -1,1 +1,20 @@
 pub(crate) mod prover;
+pub(crate) mod types;
+
+use super::polynomial::SharedPolynomial;
+use ark_ec::pairing::Pairing;
+use ark_ff::PrimeField;
+use mpc_core::traits::PrimeFieldMpcProtocol;
+
+pub(crate) struct ZeroMorphOpeningClaim<T, P: Pairing>
+where
+    T: PrimeFieldMpcProtocol<P::ScalarField>,
+{
+    pub(crate) polynomial: SharedPolynomial<T, P>,
+    pub(crate) opening_pair: OpeningPair<P::ScalarField>,
+}
+
+pub(crate) struct OpeningPair<F: PrimeField> {
+    pub(crate) challenge: F,
+    pub(crate) evaluation: F,
+}
--- co-noir/ultrahonk/src/decider/zeromorph/types.rs
+++ co-noir/ultrahonk/src/decider/zeromorph/types.rs
@@ -18,72 +18,21 @@ pub(crate) struct PolyGShift<'a, T: Default> {
     pub(crate) wires: &'a ShiftedWitnessEntities<T>,
 }
 
-#[allow(unused)]
 impl<'a, T: Default> PolyF<'a, T> {
     pub(crate) fn iter(&self) -> impl Iterator<Item = &T> {
         self.precomputed.iter().chain(self.witness.iter())
     }
 }
 
-#[allow(unused)]
 impl<'a, T: Default> PolyG<'a, T> {
-    // Table Polys
-    const TABLE_1: usize = 0;
-    const TABLE_2: usize = 1;
-    const TABLE_3: usize = 2;
-    const TABLE_4: usize = 3;
-
-    // Wire Polys
-    const W_L: usize = 0;
-    const W_R: usize = 1;
-    const W_O: usize = 2;
-    const W_4: usize = 3;
-
     pub(crate) fn iter(&self) -> impl Iterator<Item = &T> {
         self.tables
             .into_iter()
             .chain(self.wires)
             .chain(std::iter::once(self.z_perm))
     }
-
-    pub(crate) fn table_1(&self) -> &T {
-        self.tables[Self::TABLE_1]
-    }
-
-    pub(crate) fn table_2(&self) -> &T {
-        self.tables[Self::TABLE_2]
-    }
-
-    pub(crate) fn table_3(&self) -> &T {
-        self.tables[Self::TABLE_3]
-    }
-
-    pub(crate) fn table_4(&self) -> &T {
-        self.tables[Self::TABLE_4]
-    }
-
-    pub(crate) fn w_l(&self) -> &T {
-        self.wires[Self::W_L]
-    }
-
-    pub(crate) fn w_r(&self) -> &T {
-        self.wires[Self::W_R]
-    }
-
-    pub(crate) fn w_o(&self) -> &T {
-        self.wires[Self::W_O]
-    }
-
-    pub(crate) fn w_4(&self) -> &T {
-        self.wires[Self::W_4]
-    }
-
-    pub(crate) fn z_perm(&self) -> &T {
-        self.z_perm
-    }
 }
 
-#[allow(unused)]
 impl<'a, T: Default> PolyGShift<'a, T> {
     pub(crate) fn iter(&self) -> impl Iterator<Item = &T> {
         self.tables.iter().chain(self.wires.iter())
