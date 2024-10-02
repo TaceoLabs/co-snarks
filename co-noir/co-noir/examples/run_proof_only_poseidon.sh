2cccae0256bf0fdff38cfa8c34f4ba2e1caf33fc
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-10-02 08:41:20 +0200

    .

--- co-noir/co-noir/examples/run_proof_only_poseidon.sh
+++ co-noir/co-noir/examples/run_proof_only_poseidon.sh
@@ -1,5 +1,5 @@
 # split input into shares
-cargo run --release --bin co-noir -- split-witness --witness test_vectors/poseidon/poseidon.gz --protocol REP3 --out-dir test_vectors/poseidon
+cargo run --release --bin co-noir -- split-witness --witness test_vectors/poseidon/poseidon.gz --circuit test_vectors/poseidon/poseidon.json --protocol REP3 --out-dir test_vectors/poseidon
 # run proving in MPC
 cargo run --release --bin co-noir -- generate-proof --witness test_vectors/poseidon/witness.wtns.0.shared --protocol REP3 --config configs/party1.toml --out proof.0.json &
 cargo run --release --bin co-noir -- generate-proof --witness test_vectors/poseidon/witness.wtns.1.shared --protocol REP3 --config configs/party2.toml --out proof.1.json &
