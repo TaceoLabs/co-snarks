e43534aafefb4811bdf1f7fe1fa1493ab5c9152d
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-10-03 12:32:15 +0200

    fix: clippy

--- co-noir/ultrahonk/src/lib.rs
+++ co-noir/ultrahonk/src/lib.rs
@@ -26,7 +26,7 @@ use types::ProverCrs;
 
 pub const NUM_ALPHAS: usize = decider::relations::NUM_SUBRELATIONS - 1;
 /// The log of the max circuit size assumed in order to achieve constant sized Honk proofs
-/// TODO(https://github.com/AztecProtocol/barretenberg/issues/1046): Remove the need for const sized proofs
+/// TODO(<https://github.com/AztecProtocol/barretenberg/issues/1046>): Remove the need for const sized proofs
 pub const CONST_PROOF_SIZE_LOG_N: usize = 28;
 pub const N_MAX: usize = 1 << 25;
 
