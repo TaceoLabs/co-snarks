924585dcd5c76a436c1c3828c8108f6194f52b6e
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-10-03 10:50:01 +0200

    .

--- co-noir/co-noir/examples/run_full_poseidon_shamir.sh
+++ co-noir/co-noir/examples/run_full_poseidon_shamir.sh
@@ -12,5 +12,7 @@ cargo run --release --bin co-noir -- translate-witness --witness test_vectors/po
 cargo run --release --bin co-noir -- generate-proof --witness test_vectors/poseidon/shamir_poseidon.gz.0.shared --circuit test_vectors/poseidon/poseidon.json --crs test_vectors/bn254_g1.dat --protocol SHAMIR --config configs/party1.toml --out proof.0.proof --public-input public_input.json &
 cargo run --release --bin co-noir -- generate-proof --witness test_vectors/poseidon/shamir_poseidon.gz.1.shared --circuit test_vectors/poseidon/poseidon.json --crs test_vectors/bn254_g1.dat --protocol SHAMIR --config configs/party2.toml --out proof.1.proof &
 cargo run --release --bin co-noir -- generate-proof --witness test_vectors/poseidon/shamir_poseidon.gz.2.shared --circuit test_vectors/poseidon/poseidon.json --crs test_vectors/bn254_g1.dat --protocol SHAMIR --config configs/party3.toml --out proof.2.proof
+# Create verification key
+cargo run --release --bin co-noir -- create-vk --circuit test_vectors/poseidon/poseidon.json --crs test_vectors/bn254_g1.dat --vk test_vectors/poseidon/verification_key
 # verify proof
-# cargo run --release --bin co-circom -- verify groth16 --proof proof.0.json --vk test_vectors/poseidon/verification_key.json --public-input public_input.json --curve BN254
+cargo run --release --bin co-noir -- verify --proof proof.0.json --vk test_vectors/poseidon/verification_key --crs test_vectors/bn254_g2.dat
