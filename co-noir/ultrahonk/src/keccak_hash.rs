b9833fa2a1305c9ab067387bd4cef1e416a816ef
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-10-07 11:07:43 +0200

    untested keccak hash

--- co-noir/ultrahonk/src/keccak_hash.rs
+++ co-noir/ultrahonk/src/keccak_hash.rs
@@ -1,9 +1,17 @@
-use crate::prelude::TranscriptHasher;
+use crate::{prelude::TranscriptHasher, Serialize};
 use ark_ff::PrimeField;
-use sha3::Keccak256;
+use sha3::{Digest, Keccak256};
 
 impl<F: PrimeField> TranscriptHasher<F> for Keccak256 {
     fn hash(buffer: Vec<F>) -> F {
-        todo!()
+        // Losing 2 bits of this is not an issue -> we can just reduce mod p
+
+        let vec = Serialize::to_buffer(&buffer, false);
+        let mut hasher = Keccak256::default();
+        hasher.update(vec);
+        let hash_result = hasher.finalize();
+
+        let mut offset = 0;
+        Serialize::read_field_element(&hash_result, &mut offset)
     }
 }
