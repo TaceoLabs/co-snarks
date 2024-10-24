08f8fad41f70da489199623e077568a8bd2a9bee
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-10-24 09:29:56 +0200

    feat: Add functionality to decompose a shared fieldelement with yao

--- mpc-core/src/protocols/rep3/yao.rs
+++ mpc-core/src/protocols/rep3/yao.rs
@@ -14,8 +14,11 @@ use super::{
 };
 use crate::protocols::rep3::id::PartyID;
 use ark_ff::{PrimeField, Zero};
+use circuits::GarbledCircuits;
+use evaluator::Rep3Evaluator;
 use fancy_garbling::{hash_wires, util::tweak2, BinaryBundle, WireLabel, WireMod2};
-use itertools::Itertools;
+use garbler::Rep3Garbler;
+use itertools::{izip, Itertools};
 use num_bigint::BigUint;
 use rand::{CryptoRng, Rng};
 use scuttlebutt::Block;
@@ -219,7 +222,7 @@ impl GCUtils {
     }
 
     /// Converts bits into a field element
-    pub fn bits_to_field<F: PrimeField>(bits: Vec<bool>) -> IoResult<F> {
+    pub fn bits_to_field<F: PrimeField>(bits: &[bool]) -> IoResult<F> {
         let mut res = BigUint::zero();
         for bit in bits.iter().rev() {
             res <<= 1;
@@ -287,11 +290,11 @@ impl GCUtils {
     }
 
     /// This puts the X_0 values into garbler_wires and X_c values into evaluator_wires
-    fn encode_bits<R: Rng + CryptoRng>(
+    fn encode_bits_as_wires<R: Rng + CryptoRng>(
         bits: Vec<u16>,
         rng: &mut R,
         delta: WireMod2,
-    ) -> GCInputs<WireMod2> {
+    ) -> (Vec<WireMod2>, Vec<WireMod2>) {
         let mut garbler_wires = Vec::with_capacity(bits.len());
         let mut evaluator_wires = Vec::with_capacity(bits.len());
         for bit in bits {
@@ -299,11 +302,30 @@ impl GCUtils {
             garbler_wires.push(mine);
             evaluator_wires.push(theirs);
         }
+        (garbler_wires, evaluator_wires)
+    }
+
+    /// Makes a GCInput out of the wires
+    fn wires_to_gcinput(
+        garbler_wires: Vec<WireMod2>,
+        evaluator_wires: Vec<WireMod2>,
+        delta: WireMod2,
+    ) -> GCInputs<WireMod2> {
         GCInputs {
             garbler_wires: BinaryBundle::new(garbler_wires),
             evaluator_wires: BinaryBundle::new(evaluator_wires),
             delta,
         }
+    }
+
+    /// This puts the X_0 values into garbler_wires and X_c values into evaluator_wires
+    fn encode_bits<R: Rng + CryptoRng>(
+        bits: Vec<u16>,
+        rng: &mut R,
+        delta: WireMod2,
+    ) -> GCInputs<WireMod2> {
+        let (garbler_wires, evaluator_wires) = Self::encode_bits_as_wires(bits, rng, delta);
+        Self::wires_to_gcinput(garbler_wires, evaluator_wires, delta)
     }
 
     /// This puts the X_0 values into garbler_wires and X_c values into evaluator_wires
@@ -416,16 +438,27 @@ pub fn joint_input_arithmetic_added<F: PrimeField, N: Rep3Network>(
     delta: Option<WireMod2>,
     io_context: &mut IoContext<N>,
 ) -> IoResult<[BinaryBundle<WireMod2>; 2]> {
+    joint_input_arithmetic_added_many(&[x], delta, io_context)
+}
+
+/// Transforms a vector of arithmetically shared inputs x = (x_1, x_2, x_3) into two yao shares x_1^Y, (x_2 + x_3)^Y. The used delta is an input to the function to allow for the same delta to be used for multiple conversions.
+fn joint_input_arithmetic_added_many<F: PrimeField, N: Rep3Network>(
+    x: &[Rep3PrimeFieldShare<F>],
+    delta: Option<WireMod2>,
+    io_context: &mut IoContext<N>,
+) -> IoResult<[BinaryBundle<WireMod2>; 2]> {
     let id = io_context.id;
+    let n_inputs = x.len();
     let n_bits = F::MODULUS_BIT_SIZE as usize;
+    let bits = n_inputs * n_bits;
 
     let (x01, x2) = match id {
         PartyID::ID0 => {
             // Receive x0
-            let x01 = GCUtils::receive_bundle_from(n_bits, &mut io_context.network, PartyID::ID1)?;
+            let x01 = GCUtils::receive_bundle_from(bits, &mut io_context.network, PartyID::ID1)?;
 
             // Receive x2
-            let x2 = GCUtils::receive_bundle_from(n_bits, &mut io_context.network, PartyID::ID2)?;
+            let x2 = GCUtils::receive_bundle_from(bits, &mut io_context.network, PartyID::ID2)?;
             (x01, x2)
         }
         PartyID::ID1 => {
@@ -437,16 +470,26 @@ pub fn joint_input_arithmetic_added<F: PrimeField, N: Rep3Network>(
                 ))?,
             };
 
+            let mut garbler_bundle = Vec::with_capacity(bits);
+            let mut evaluator_bundle = Vec::with_capacity(bits);
+
             // Input x01
-            let sum = x.a + x.b;
-            let x01 = GCUtils::encode_field(sum, &mut io_context.rng, delta);
+            for x in x.iter() {
+                let sum = x.a + x.b;
+                let bits = GCUtils::field_to_bits_as_u16(sum);
+                let (garbler, evaluator) =
+                    GCUtils::encode_bits_as_wires(bits, &mut io_context.rng, delta);
+                garbler_bundle.extend(garbler);
+                evaluator_bundle.extend(evaluator);
+            }
+            let x01 = GCUtils::wires_to_gcinput(garbler_bundle, evaluator_bundle, delta);
 
             // Send x01 to the other parties
             GCUtils::send_inputs(&x01, &mut io_context.network, PartyID::ID2)?;
             let x01 = x01.garbler_wires;
 
             // Receive x2
-            let x2 = GCUtils::receive_bundle_from(n_bits, &mut io_context.network, PartyID::ID2)?;
+            let x2 = GCUtils::receive_bundle_from(bits, &mut io_context.network, PartyID::ID2)?;
             (x01, x2)
         }
         PartyID::ID2 => {
@@ -458,15 +501,25 @@ pub fn joint_input_arithmetic_added<F: PrimeField, N: Rep3Network>(
                 ))?,
             };
 
+            let mut garbler_bundle = Vec::with_capacity(bits);
+            let mut evaluator_bundle = Vec::with_capacity(bits);
+
             // Input x2
-            let x2 = GCUtils::encode_field(x.a, &mut io_context.rng, delta);
+            for x in x.iter() {
+                let bits = GCUtils::field_to_bits_as_u16(x.a);
+                let (garbler, evaluator) =
+                    GCUtils::encode_bits_as_wires(bits, &mut io_context.rng, delta);
+                garbler_bundle.extend(garbler);
+                evaluator_bundle.extend(evaluator);
+            }
+            let x2 = GCUtils::wires_to_gcinput(garbler_bundle, evaluator_bundle, delta);
 
             // Send x2 to the other parties
             GCUtils::send_inputs(&x2, &mut io_context.network, PartyID::ID1)?;
             let x2 = x2.garbler_wires;
 
             // Receive x01
-            let x01 = GCUtils::receive_bundle_from(n_bits, &mut io_context.network, PartyID::ID1)?;
+            let x01 = GCUtils::receive_bundle_from(bits, &mut io_context.network, PartyID::ID1)?;
             (x01, x2)
         }
     };
@@ -538,19 +591,21 @@ pub fn joint_input_binary_xored<F: PrimeField, N: Rep3Network>(
     Ok([x01, x2])
 }
 
-/// Lets the party with id2 input a field element, which gets shared as Yao wires to the other parties.
-pub fn input_field_id2<F: PrimeField, N: Rep3Network>(
-    x: Option<F>,
+/// Lets the party with id2 input a vector field elements, which gets shared as Yao wires to the other parties.
+fn input_field_id2_many<F: PrimeField, N: Rep3Network>(
+    x: Option<Vec<F>>,
     delta: Option<WireMod2>,
+    n_inputs: usize,
     io_context: &mut IoContext<N>,
 ) -> IoResult<BinaryBundle<WireMod2>> {
     let id = io_context.id;
     let n_bits = F::MODULUS_BIT_SIZE as usize;
+    let bits = n_inputs * n_bits;
 
     let x = match id {
         PartyID::ID0 | PartyID::ID1 => {
             // Receive x
-            GCUtils::receive_bundle_from(n_bits, &mut io_context.network, PartyID::ID2)?
+            GCUtils::receive_bundle_from(bits, &mut io_context.network, PartyID::ID2)?
         }
         PartyID::ID2 => {
             let delta = match delta {
@@ -569,7 +624,25 @@ pub fn input_field_id2<F: PrimeField, N: Rep3Network>(
                 ))?,
             };
 
-            let x = GCUtils::encode_field(x, &mut io_context.rng, delta);
+            if x.len() != n_inputs {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Invalid number of inputs",
+                ));
+            }
+
+            let mut garbler_bundle = Vec::with_capacity(bits);
+            let mut evaluator_bundle = Vec::with_capacity(bits);
+
+            // Input x1
+            for x in x {
+                let bits = GCUtils::field_to_bits_as_u16(x);
+                let (garbler, evaluator) =
+                    GCUtils::encode_bits_as_wires(bits, &mut io_context.rng, delta);
+                garbler_bundle.extend(garbler);
+                evaluator_bundle.extend(evaluator);
+            }
+            let x = GCUtils::wires_to_gcinput(garbler_bundle, evaluator_bundle, delta);
 
             // Send x to the other parties
             GCUtils::send_inputs(&x, &mut io_context.network, PartyID::ID1)?;
@@ -577,4 +650,153 @@ pub fn input_field_id2<F: PrimeField, N: Rep3Network>(
         }
     };
     Ok(x)
+}
+
+/// Lets the party with id2 input a field element, which gets shared as Yao wires to the other parties.
+pub fn input_field_id2<F: PrimeField, N: Rep3Network>(
+    x: Option<F>,
+    delta: Option<WireMod2>,
+    io_context: &mut IoContext<N>,
+) -> IoResult<BinaryBundle<WireMod2>> {
+    let x = x.map(|x| vec![x]);
+    input_field_id2_many(x, delta, 1, io_context)
+}
+
+/// Decomposes a shared field element into chunks, which are also represented as shared field elements. Per field element, the total bit size of the shared chunks is given by total_bit_size_per_field, whereas each chunk has at most (i.e, the last chunk can be smaller) decompose_bit_size bits.
+pub fn decompose_arithmetic<F: PrimeField, N: Rep3Network>(
+    input: Rep3PrimeFieldShare<F>,
+    io_context: &mut IoContext<N>,
+    total_bit_size_per_field: usize,
+    decompose_bit_size: usize,
+) -> IoResult<Vec<Rep3PrimeFieldShare<F>>> {
+    decompose_arithmetic_many(
+        &[input],
+        io_context,
+        total_bit_size_per_field,
+        decompose_bit_size,
+    )
+}
+
+// TODO implement with streaming Garbler/Evaluator as well
+// TODO implement with a2b/b2a as well
+
+/// Decomposes a vector of shared field element into chunks, which are also represented as shared field elements. Per field element, the total bit size of the shared chunks is given by total_bit_size_per_field, whereas each chunk has at most (i.e, the last chunk can be smaller) decompose_bit_size bits.
+pub fn decompose_arithmetic_many<F: PrimeField, N: Rep3Network>(
+    inputs: &[Rep3PrimeFieldShare<F>],
+    io_context: &mut IoContext<N>,
+    total_bit_size_per_field: usize,
+    decompose_bit_size: usize,
+) -> IoResult<Vec<Rep3PrimeFieldShare<F>>> {
+    let num_inputs = inputs.len();
+    let num_decomps_per_field = total_bit_size_per_field.div_ceil(decompose_bit_size);
+    let total_output_elements = num_decomps_per_field * num_inputs;
+
+    let delta = io_context.rngs.generate_random_garbler_delta(io_context.id);
+
+    let [x01, x2] = joint_input_arithmetic_added_many(inputs, delta, io_context)?;
+
+    let mut res = vec![Rep3PrimeFieldShare::zero_share(); total_output_elements];
+
+    match io_context.id {
+        PartyID::ID0 => {
+            for res in res.iter_mut() {
+                let k3 = io_context.rngs.bitcomp2.random_fes_3keys::<F>();
+                res.b = (k3.0 + k3.1 + k3.2).neg();
+            }
+
+            // TODO this can be parallelized with joint_input_arithmetic_added_many
+            let x23 = input_field_id2_many::<F, _>(None, None, total_output_elements, io_context)?;
+
+            let mut evaluator = Rep3Evaluator::new(io_context);
+            evaluator.receive_circuit()?;
+
+            let x1 = GarbledCircuits::decompose_field_element_many::<_, F>(
+                &mut evaluator,
+                &x01,
+                &x2,
+                &x23,
+                decompose_bit_size,
+                total_bit_size_per_field,
+            );
+            let x1 = GCUtils::garbled_circuits_error(x1)?;
+            let x1 = evaluator.output_to_id0_and_id1(x1.wires())?;
+
+            // Compose the bits
+            for (res, x1) in izip!(res.iter_mut(), x1.chunks(F::MODULUS_BIT_SIZE as usize)) {
+                res.a = GCUtils::bits_to_field(x1)?;
+            }
+        }
+        PartyID::ID1 => {
+            for res in res.iter_mut() {
+                let k2 = io_context.rngs.bitcomp1.random_fes_3keys::<F>();
+                res.a = (k2.0 + k2.1 + k2.2).neg();
+            }
+
+            // TODO this can be parallelized with joint_input_arithmetic_added_many
+            let x23 = input_field_id2_many::<F, _>(None, None, total_output_elements, io_context)?;
+
+            let mut garbler =
+                Rep3Garbler::new_with_delta(io_context, delta.expect("Delta not provided"));
+
+            let x1 = GarbledCircuits::decompose_field_element_many::<_, F>(
+                &mut garbler,
+                &x01,
+                &x2,
+                &x23,
+                decompose_bit_size,
+                total_bit_size_per_field,
+            );
+            let x1 = GCUtils::garbled_circuits_error(x1)?;
+            let x1 = garbler.output_to_id0_and_id1(x1.wires())?;
+            let x1 = match x1 {
+                Some(x1) => x1,
+                None => Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "No output received",
+                ))?,
+            };
+
+            // Compose the bits
+            for (res, x1) in izip!(res.iter_mut(), x1.chunks(F::MODULUS_BIT_SIZE as usize)) {
+                res.b = GCUtils::bits_to_field(x1)?;
+            }
+        }
+        PartyID::ID2 => {
+            let mut x23 = Vec::with_capacity(total_output_elements);
+            for res in res.iter_mut() {
+                let k2 = io_context.rngs.bitcomp1.random_fes_3keys::<F>();
+                let k3 = io_context.rngs.bitcomp2.random_fes_3keys::<F>();
+                let k2_comp = k2.0 + k2.1 + k2.2;
+                let k3_comp = k3.0 + k3.1 + k3.2;
+                x23.push(k2_comp + k3_comp);
+                res.a = k3_comp.neg();
+                res.b = k2_comp.neg();
+            }
+
+            // TODO this can be parallelized with joint_input_arithmetic_added_many
+            let x23 = input_field_id2_many(Some(x23), delta, total_output_elements, io_context)?;
+
+            let mut garbler =
+                Rep3Garbler::new_with_delta(io_context, delta.expect("Delta not provided"));
+
+            let x1 = GarbledCircuits::decompose_field_element_many::<_, F>(
+                &mut garbler,
+                &x01,
+                &x2,
+                &x23,
+                decompose_bit_size,
+                total_bit_size_per_field,
+            );
+            let x1 = GCUtils::garbled_circuits_error(x1)?;
+            let x1 = garbler.output_to_id0_and_id1(x1.wires())?;
+            if x1.is_some() {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Unexpected output received",
+                ))?;
+            }
+        }
+    }
+
+    Ok(res)
 }
