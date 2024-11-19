f49133b725389acca1adda41d556e91cf191f68d
Florin F <156660445+florin5f@users.noreply.github.com>
2024-11-19 11:10:49 +0100

    .

---
+++ co-noir/co-noir/examples/run_full_poseidon_assert.sh
@@ -0,0 +1,21 @@
+# split input into shares
+cargo run --release --bin co-noir -- split-input --circuit test_vectors/poseidon_assert/poseidon_assert.json --input test_vectors/poseidon_assert/Alice.toml --protocol REP3 --out-dir test_vectors/poseidon_assert
+cargo run --release --bin co-noir -- split-input --circuit test_vectors/poseidon_assert/poseidon_assert.json --input test_vectors/poseidon_assert/Bob.toml --protocol REP3 --out-dir test_vectors/poseidon_assert
+# merge inputs into single input file
+cargo run --release --bin co-noir -- merge-input-shares --inputs test_vectors/poseidon_assert/Alice.toml.0.shared --inputs test_vectors/poseidon_assert/Bob.toml.0.shared --protocol REP3 --out test_vectors/poseidon_assert/Prover.toml.0.shared
+cargo run --release --bin co-noir -- merge-input-shares --inputs test_vectors/poseidon_assert/Alice.toml.1.shared --inputs test_vectors/poseidon_assert/Bob.toml.1.shared --protocol REP3 --out test_vectors/poseidon_assert/Prover.toml.1.shared
+cargo run --release --bin co-noir -- merge-input-shares --inputs test_vectors/poseidon_assert/Alice.toml.2.shared --inputs test_vectors/poseidon_assert/Bob.toml.2.shared --protocol REP3 --out test_vectors/poseidon_assert/Prover.toml.2.shared
+# run witness extension in MPC
+cargo run --release --bin co-noir -- generate-witness --input test_vectors/poseidon_assert/Prover.toml.0.shared --circuit test_vectors/poseidon_assert/poseidon_assert.json --protocol REP3 --config configs/party1.toml --out test_vectors/poseidon_assert/poseidon_assert.gz.0.shared &
+cargo run --release --bin co-noir -- generate-witness --input test_vectors/poseidon_assert/Prover.toml.1.shared --circuit test_vectors/poseidon_assert/poseidon_assert.json --protocol REP3 --config configs/party2.toml --out test_vectors/poseidon_assert/poseidon_assert.gz.1.shared &
+cargo run --release --bin co-noir -- generate-witness --input test_vectors/poseidon_assert/Prover.toml.2.shared --circuit test_vectors/poseidon_assert/poseidon_assert.json --protocol REP3 --config configs/party3.toml --out test_vectors/poseidon_assert/poseidon_assert.gz.2.shared
+wait $(jobs -p)
+# run proving in MPC
+cargo run --release --bin co-noir -- build-and-generate-proof --witness test_vectors/poseidon_assert/poseidon_assert.gz.0.shared --circuit test_vectors/poseidon_assert/poseidon_assert.json --crs test_vectors/bn254_g1.dat --protocol REP3 --hasher KECCAK --config configs/party1.toml --out proof.0.proof --public-input public_input.json &
+cargo run --release --bin co-noir -- build-and-generate-proof --witness test_vectors/poseidon_assert/poseidon_assert.gz.1.shared --circuit test_vectors/poseidon_assert/poseidon_assert.json --crs test_vectors/bn254_g1.dat --protocol REP3 --hasher KECCAK --config configs/party2.toml --out proof.1.proof &
+cargo run --release --bin co-noir -- build-and-generate-proof --witness test_vectors/poseidon_assert/poseidon_assert.gz.2.shared --circuit test_vectors/poseidon_assert/poseidon_assert.json --crs test_vectors/bn254_g1.dat --protocol REP3 --hasher KECCAK --config configs/party3.toml --out proof.2.proof
+wait $(jobs -p)
+# Create verification key
+cargo run --release --bin co-noir -- create-vk --circuit test_vectors/poseidon_assert/poseidon_assert.json --crs test_vectors/bn254_g1.dat --hasher KECCAK --vk test_vectors/poseidon_assert/verification_key
+# verify proof
+cargo run --release --bin co-noir -- verify --proof proof.0.proof --vk test_vectors/poseidon_assert/verification_key --hasher KECCAK --crs test_vectors/bn254_g2.dat
