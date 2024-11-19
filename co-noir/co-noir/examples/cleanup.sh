d2f7aa465830f751e81d5931bea41d3a102fd286
Florin F <156660445+florin5f@users.noreply.github.com>
2024-11-19 17:12:24 +0100

    works

--- co-noir/co-noir/examples/cleanup.sh
+++ co-noir/co-noir/examples/cleanup.sh
@@ -9,3 +9,7 @@ find . -name "verification_key" -type f -delete
 find . -name "proof.bb*" -type f -delete
 # delete all bb vk files
 find . -name "vk.bb*" -type f -delete
+# delete all proofs and vks in test_vectors files
+cd test_vectors
+find . -name "vk" -type f -delete
+find . -name "proof" -type f -delete
