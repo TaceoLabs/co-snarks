d90a176d131d729f9470a5f831981c284bbc6315
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-11-20 09:46:45 +0100

    .

--- co-noir/co-noir/examples/compare.sh
+++ co-noir/co-noir/examples/compare.sh
@@ -1,16 +1,35 @@
 export CARGO_TERM_QUIET=true
+BARRETENBERG_BINARY=~/.bb/bb  ##specify the $BARRETENBERG_BINARY path here
+
 NARGO_VERSION=38 ##specify the desired nargo version here
 BARRETENBERG_VERSION=62 ##specify the desired barretenberg version here or use the corresponding one for this nargo version
 PLAINDRIVER="../../../target/release/plaindriver"
 exit_code=0
-bash -c "noirup -v 0.${NARGO_VERSION}.0"
+
+# build the plaindriver binary
+cargo build --release --bin plaindriver
+
+## install noirup: curl -L https://raw.githubusercontent.com/noir-lang/noirup/main/install | bash
+r=$(bash -c "nargo --version")
+if  [[ $r != "nargo version = 0.$NARGO_VERSION.0"* ]];
+then
+    bash -c "noirup -v 0.${NARGO_VERSION}.0"
+fi
 
 ## use one of these two methods
-bash -c "bbup -nv 0.${NARGO_VERSION}.0"
-# bash -c "bbup -v 0.${BARRETENBERG_VERSION}.0" 
+## install bbup: curl -L bbup.dev | bash
+# bash -c "bbup -nv 0.${NARGO_VERSION}.0"
+r=$(bash -c "$BARRETENBERG_BINARY --version")
+if  [[ $r != "0.$BARRETENBERG_VERSION.0" ]];
+then
+    bash -c "bbup -v 0.${BARRETENBERG_VERSION}.0"
+fi
+
+echo "Using nargo version 0.$NARGO_VERSION.0"
+echo "Using bb version 0.$BARRETENBERG_VERSION.0"
+echo ""
 
 test_cases=("add3" "add3_assert" "add3u64" "mul3u64" "poseidon" "poseidon_input2")
-
 
 run_proof_verification() {
   local name=$1
@@ -30,9 +49,9 @@ run_proof_verification() {
     verify_command="verify_ultra_keccak_honk"
   fi
 
- echo "comparing" $name "with bb version:" $BARRETENBERG_VERSION " and $algorithm transcript"
+ echo "comparing" $name "with bb and $algorithm transcript"
 
-    bb $prove_command -b test_vectors/${name}/target/${name}.json -w test_vectors/${name}/target/${name}.gz -o test_vectors/${name}/${proof_file}
+    $BARRETENBERG_BINARY $prove_command -b test_vectors/${name}/target/${name}.json -w test_vectors/${name}/target/${name}.gz -o test_vectors/${name}/${proof_file} > /dev/null 2>&1
 
     diff test_vectors/${name}/${proof_file} test_vectors/${name}/proof
     if [[ $? -ne 0 ]]; then
@@ -40,26 +59,26 @@ run_proof_verification() {
     echo "::error::$name diff check of proofs failed"
   fi
 
-    bb $write_command -b test_vectors/${name}/target/${name}.json -o test_vectors/${name}/${vk_file}
+    $BARRETENBERG_BINARY $write_command -b test_vectors/${name}/target/${name}.json -o test_vectors/${name}/${vk_file} > /dev/null 2>&1
 
-    bb $verify_command -p test_vectors/${name}/proof -k test_vectors/${name}/vk
+    $BARRETENBERG_BINARY $verify_command -p test_vectors/${name}/proof -k test_vectors/${name}/vk
     if [[ $? -ne 0 ]]; then
     exit_code=1
     echo "::error::$name verifying with bb, our proof and our key failed"
   fi
 
-    bb $verify_command -p test_vectors/${name}/proof -k test_vectors/${name}/${vk_file}
+    $BARRETENBERG_BINARY $verify_command -p test_vectors/${name}/proof -k test_vectors/${name}/${vk_file}
     if [[ $? -ne 0 ]]; then
     exit_code=1
     echo "::error::$name verifying with bb, our proof and their key failed"
   fi
 
-    bb $verify_command -p test_vectors/${name}/${proof_file} -k test_vectors/${name}/vk
+    $BARRETENBERG_BINARY $verify_command -p test_vectors/${name}/${proof_file} -k test_vectors/${name}/vk
     if [[ $? -ne 0 ]]; then
     exit_code=1
     echo "::error::$name verifying with bb, their proof and our key failed"
     fi
-  bb $verify_command -p test_vectors/${name}/${proof_file} -k test_vectors/${name}/${vk_file}
+  $BARRETENBERG_BINARY $verify_command -p test_vectors/${name}/${proof_file} -k test_vectors/${name}/${vk_file}
     if [[ $? -ne 0 ]]; then
     exit_code=1
     echo "::error::$name verifying with bb, their proof and their key failed"
@@ -72,13 +91,13 @@ for f in "${test_cases[@]}"; do
   echo "running ultrahonk example" $f
 
   failed=0
-  
+
   # compile witnesses and bytecode with specified nargo version
     echo "computing witnesses with nargo"
-  bash -c "(cd test_vectors/${f} && nargo execute)" 
+  bash -c "(cd test_vectors/${f} && nargo execute)"  > /dev/null 2>&1
 
   # -e to exit on first error
-  bash -c "${PLAINDRIVER} --prover-crs test_vectors/bn254_g1.dat --verifier-crs test_vectors/bn254_g2.dat --input test_vectors/${f}/Prover.toml --circuit test_vectors/${f}/target/${f}.json --hasher POSEIDON --out-dir test_vectors/${f}" || failed=1
+  bash -c "${PLAINDRIVER} --prover-crs test_vectors/bn254_g1.dat --verifier-crs test_vectors/bn254_g2.dat --input test_vectors/${f}/Prover.toml --circuit test_vectors/${f}/target/${f}.json --hasher POSEIDON --out-dir test_vectors/${f}" > /dev/null 2>&1 || failed=1
 
   if [ "$failed" -ne 0 ]
   then
@@ -89,7 +108,7 @@ for f in "${test_cases[@]}"; do
   bash cleanup.sh
 
    # -e to exit on first error
-  bash -c "${PLAINDRIVER} --prover-crs test_vectors/bn254_g1.dat --verifier-crs test_vectors/bn254_g2.dat --input test_vectors/${f}/Prover.toml --circuit test_vectors/${f}/target/${f}.json --hasher KECCAK --out-dir test_vectors/${f}" || failed=1
+  bash -c "${PLAINDRIVER} --prover-crs test_vectors/bn254_g1.dat --verifier-crs test_vectors/bn254_g2.dat --input test_vectors/${f}/Prover.toml --circuit test_vectors/${f}/target/${f}.json --hasher KECCAK --out-dir test_vectors/${f}" > /dev/null 2>&1  || failed=1
 
   if [ "$failed" -ne 0 ]
   then
@@ -98,7 +117,7 @@ for f in "${test_cases[@]}"; do
   fi
   run_proof_verification "$f" "keccak"
   bash cleanup.sh
+  echo ""
 done
 
 exit "$exit_code"
-
