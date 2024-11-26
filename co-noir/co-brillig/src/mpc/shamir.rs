73b081a8431be48cc4700155570cc5c39fe0559c
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-11-26 14:22:01 +0100

    .

--- co-noir/co-brillig/src/mpc/shamir.rs
+++ co-noir/co-brillig/src/mpc/shamir.rs
@@ -1,45 +1,76 @@
-use std::marker::PhantomData;
-
+use super::{BrilligDriver, PlainBrilligDriver};
 use ark_ff::PrimeField;
 use brillig::{BitSize, IntegerBitSize};
+use mpc_core::protocols::shamir::{network::ShamirNetwork, ShamirPrimeFieldShare, ShamirProtocol};
+use std::marker::PhantomData;
 
-use super::BrilligDriver;
+use super::PlainBrilligType as Public;
 
-/// For now only a stub. Do we really want this?
-#[derive(Default)]
-pub struct ShamirBrilligDriver<F: PrimeField> {
+/// A driver for the coBrillig-VM that uses Shamir secret sharing.
+pub struct ShamirBrilligDriver<F: PrimeField, N: ShamirNetwork> {
+    protocol: ShamirProtocol<F, N>,
+    plain_driver: PlainBrilligDriver<F>,
     phantom_data: PhantomData<F>,
 }
 
-/// For now only a stub. Do we really want this?
-#[derive(Clone, Default, Debug, PartialEq)]
-pub struct ShamirBrilligType<F: PrimeField> {
-    phantom_data: PhantomData<F>,
+/// The types for the coBrillig
+/// Shamir driver. The values
+/// can either be shared or public.
+#[derive(Clone, Debug, PartialEq)]
+pub enum ShamirBrilligType<F: PrimeField> {
+    /// A public value
+    Public(Public<F>),
+    /// A shared value.
+    /// TODO for now we only support prime fields
+    Shared(ShamirPrimeFieldShare<F>),
 }
 
 impl<F: PrimeField> From<F> for ShamirBrilligType<F> {
-    fn from(_value: F) -> Self {
-        todo!()
+    fn from(value: F) -> Self {
+        ShamirBrilligType::Public(Public::Field(value))
     }
 }
 
-impl<F: PrimeField> BrilligDriver<F> for ShamirBrilligDriver<F> {
+impl<F: PrimeField> Default for ShamirBrilligType<F> {
+    fn default() -> Self {
+        Self::from(F::default())
+    }
+}
+
+impl<F: PrimeField, N: ShamirNetwork> BrilligDriver<F> for ShamirBrilligDriver<F, N> {
     type BrilligType = ShamirBrilligType<F>;
 
-    fn cast(&self, _src: Self::BrilligType, _bit_size: BitSize) -> eyre::Result<Self::BrilligType> {
-        todo!()
+    fn cast(&self, val: Self::BrilligType, bit_size: BitSize) -> eyre::Result<Self::BrilligType> {
+        if let ShamirBrilligType::Public(public) = val {
+            let casted = self.plain_driver.cast(public, bit_size)?;
+            Ok(ShamirBrilligType::Public(casted))
+        } else {
+            eyre::bail!("Cannot cast shared value with Shamir")
+        }
     }
 
-    fn try_into_usize(_val: Self::BrilligType) -> eyre::Result<usize> {
-        todo!()
+    fn try_into_usize(val: Self::BrilligType) -> eyre::Result<usize> {
+        // for now we only support casting public values to usize
+        // we return an error if we call this on a shared value
+        if let ShamirBrilligType::Public(public) = val {
+            PlainBrilligDriver::try_into_usize(public)
+        } else {
+            eyre::bail!("cannot convert shared value to usize")
+        }
     }
 
-    fn try_into_bool(_val: Self::BrilligType) -> eyre::Result<bool> {
-        todo!()
+    fn try_into_bool(val: Self::BrilligType) -> eyre::Result<bool> {
+        // for now we only support casting public values to bools
+        // we return an error if we call this on a shared value
+        if let ShamirBrilligType::Public(public) = val {
+            PlainBrilligDriver::try_into_bool(public)
+        } else {
+            eyre::bail!("cannot convert shared value to usize")
+        }
     }
 
-    fn public_value(_val: F, _bit_size: BitSize) -> Self::BrilligType {
-        todo!()
+    fn public_value(val: F, bit_size: BitSize) -> Self::BrilligType {
+        ShamirBrilligType::Public(PlainBrilligDriver::public_value(val, bit_size))
     }
 
     fn add(
@@ -130,22 +161,38 @@ impl<F: PrimeField> BrilligDriver<F> for ShamirBrilligDriver<F> {
 
     fn to_radix(
         &mut self,
-        _val: Self::BrilligType,
-        _radix: Self::BrilligType,
-        _output_size: usize,
-        _bits: bool,
+        val: Self::BrilligType,
+        radix: Self::BrilligType,
+        output_size: usize,
+        bits: bool,
     ) -> eyre::Result<Vec<Self::BrilligType>> {
-        todo!()
+        if let (ShamirBrilligType::Public(val), ShamirBrilligType::Public(radix)) = (val, radix) {
+            let result = self.plain_driver.to_radix(val, radix, output_size, bits)?;
+            Ok(result
+                .into_iter()
+                .map(|val| ShamirBrilligType::Public(val))
+                .collect())
+        } else {
+            eyre::bail!("Cannot use to_radix with Shamir shares")
+        }
     }
 
     fn expect_int(
-        _val: Self::BrilligType,
-        _bit_size: IntegerBitSize,
+        val: Self::BrilligType,
+        bit_size: IntegerBitSize,
     ) -> eyre::Result<Self::BrilligType> {
-        todo!()
+        if let ShamirBrilligType::Public(public) = val {
+            let result = PlainBrilligDriver::expect_int(public, bit_size)?;
+            Ok(ShamirBrilligType::Public(result))
+        } else {
+            eyre::bail!("expected int with bit size {bit_size}, but was something else")
+        }
     }
 
-    fn expect_field(_val: Self::BrilligType) -> eyre::Result<Self::BrilligType> {
-        todo!()
+    fn expect_field(val: Self::BrilligType) -> eyre::Result<Self::BrilligType> {
+        match &val {
+            ShamirBrilligType::Public(Public::Field(_)) | ShamirBrilligType::Shared(_) => Ok(val),
+            _ => eyre::bail!("expected field but got int"),
+        }
     }
 }
