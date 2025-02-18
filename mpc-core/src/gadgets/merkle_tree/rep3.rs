fde270441dfcf66eb3eca76a0bd2199deccd26d5
Daniel Kales <11509575+dkales@users.noreply.github.com>
2025-02-18 11:54:57 +0100

    fix: enable parallelization for poseidon merkle tree implementations

--- mpc-core/src/gadgets/merkle_tree/rep3.rs
+++ mpc-core/src/gadgets/merkle_tree/rep3.rs
@@ -36,14 +36,12 @@ impl<F: PrimeField, const T: usize, const D: u64> Poseidon2<F, T, D> {
         while len > 1 {
             debug_assert_eq!(len % ARITY, 0);
             len /= ARITY;
-            for inp in input.chunks_exact_mut(T).take(len) {
-                // Sponge mode
-                self.rep3_permutation_in_place_with_precomputation_packed(
-                    inp,
-                    &mut precomp,
-                    driver,
-                )?;
-            }
+            // Sponge mode
+            self.rep3_permutation_in_place_with_precomputation_packed(
+                &mut input[..T * len],
+                &mut precomp,
+                driver,
+            )?;
             // Only take first element as output and pad with 0 for sponge
             for i in 0..len / ARITY {
                 for j in 0..ARITY {
@@ -91,16 +89,17 @@ impl<F: PrimeField, const T: usize, const D: u64> Poseidon2<F, T, D> {
         while len > 1 {
             debug_assert_eq!(len % ARITY, 0);
             len /= ARITY;
-            for inp in input.chunks_exact_mut(T).take(len) {
-                // Compression mode
-                let feed_forward = inp[0];
-                self.rep3_permutation_in_place_with_precomputation_packed(
-                    inp,
-                    &mut precomp,
-                    driver,
-                )?;
-                inp[0] += feed_forward;
+            let ff = input.clone();
+            // Compression mode
+            self.rep3_permutation_in_place_with_precomputation_packed(
+                &mut input[..T * len],
+                &mut precomp,
+                driver,
+            )?;
+            for i in 0..len {
+                input[T * i] += ff[T * i];
             }
+
             // Only take first element as output and pad with 0 for compression
             for i in 0..len / ARITY {
                 for j in 0..ARITY {
