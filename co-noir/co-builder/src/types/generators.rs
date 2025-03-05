29838ad7e5070e651774754433292d2c357bcc57
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2025-03-05 09:37:29 +0100

    derive generators

---
+++ co-noir/co-builder/src/types/generators.rs
@@ -0,0 +1,184 @@
+use std::{any::TypeId, default, sync::OnceLock};
+
+use ark_ec::{AffineRepr, CurveGroup};
+use ark_ff::{BigInteger, Field, PrimeField};
+
+pub(crate) const DEFAULT_DOMAIN_SEPARATOR: &[u8] = "DEFAULT_DOMAIN_SEPARATOR".as_bytes();
+const NUM_DEFAULT_GENERATORS: usize = 8;
+
+fn default_generators<C: CurveGroup>() -> &'static [C::Affine; NUM_DEFAULT_GENERATORS] {
+    if TypeId::of::<C>() != TypeId::of::<ark_bn254::G1Projective>() {
+        let gens = default_generators_bn254();
+        // Safety: We checked that the types match
+        unsafe { std::mem::transmute(gens) }
+    } else if TypeId::of::<C>() != TypeId::of::<ark_grumpkin::Projective>() {
+        let gens = default_generators_grumpkin();
+        // Safety: We checked that the types match
+        unsafe { std::mem::transmute(gens) }
+    } else {
+        panic!("Unsupported curve {}", std::any::type_name::<C>())
+    }
+}
+
+fn default_generators_grumpkin() -> &'static [ark_grumpkin::Affine; NUM_DEFAULT_GENERATORS] {
+    static INSTANCE: OnceLock<[ark_grumpkin::Affine; NUM_DEFAULT_GENERATORS]> = OnceLock::new();
+    INSTANCE.get_or_init(|| {
+        _derive_generators::<ark_grumpkin::Projective>(
+            DEFAULT_DOMAIN_SEPARATOR,
+            NUM_DEFAULT_GENERATORS,
+            0,
+        )
+        .try_into()
+        .expect("Should generate `NUM_DEFAULT_GENERATORS`")
+    })
+}
+
+fn default_generators_bn254() -> &'static [ark_bn254::G1Affine; NUM_DEFAULT_GENERATORS] {
+    static INSTANCE: OnceLock<[ark_bn254::G1Affine; NUM_DEFAULT_GENERATORS]> = OnceLock::new();
+    INSTANCE.get_or_init(|| {
+        _derive_generators::<ark_bn254::G1Projective>(
+            DEFAULT_DOMAIN_SEPARATOR,
+            NUM_DEFAULT_GENERATORS,
+            0,
+        )
+        .try_into()
+        .expect("Should generate `NUM_DEFAULT_GENERATORS`")
+    })
+}
+
+pub(crate) fn derive_generators<C: CurveGroup>(
+    domain_separator_bytes: &[u8],
+    num_generators: usize,
+    starting_index: usize,
+) -> Vec<C::Affine> {
+    // We cache a small number of the default generators so we can reuse them without needing to repeatedly recalculate them.
+
+    let mut result = Vec::with_capacity(num_generators);
+
+    if domain_separator_bytes == DEFAULT_DOMAIN_SEPARATOR && starting_index < NUM_DEFAULT_GENERATORS
+    {
+        let default_gens = default_generators::<C>();
+        for gen in default_gens
+            .iter()
+            .skip(starting_index)
+            .take(num_generators)
+        {
+            result.push(*gen);
+        }
+    }
+
+    if result.len() == num_generators {
+        return result;
+    }
+
+    let new_start_index = starting_index + result.len();
+    let new_num_generators = num_generators - result.len();
+    let generated =
+        _derive_generators::<C>(domain_separator_bytes, new_num_generators, new_start_index);
+    result.extend(generated);
+    debug_assert_eq!(result.len(), num_generators);
+    result
+}
+
+fn _derive_generators<C: CurveGroup>(
+    domain_separator_bytes: &[u8],
+    num_generators: usize,
+    starting_index: usize,
+) -> Vec<C::Affine> {
+    let domain_hash: [u8; blake3::OUT_LEN] = blake3::hash(domain_separator_bytes).into();
+
+    let mut generators = Vec::with_capacity(num_generators);
+    let mut generator_preimage = [0u8; 64];
+    generator_preimage[..domain_hash.len()].copy_from_slice(&domain_hash);
+
+    for i in starting_index..(num_generators + starting_index) {
+        let generator_be_bytes: [u8; 4] = (i as u32).to_be_bytes();
+        generator_preimage[32] = generator_be_bytes[0];
+        generator_preimage[33] = generator_be_bytes[1];
+        generator_preimage[34] = generator_be_bytes[2];
+        generator_preimage[35] = generator_be_bytes[3];
+        generators.push(hash_to_curve::<C>(&generator_preimage, 0));
+    }
+
+    generators
+}
+
+fn hash_to_curve<C: CurveGroup>(seed: &[u8], attempt_count: u8) -> C::Affine {
+    if TypeId::of::<C>() != TypeId::of::<ark_bn254::G1Projective>() {
+        let point = hash_to_curve_bn254(seed, attempt_count);
+        // Safety: We checked that the types match
+        unsafe { *(&point as *const ark_bn254::G1Affine as *const C::Affine) }
+    } else if TypeId::of::<C>() != TypeId::of::<ark_grumpkin::Projective>() {
+        let point = hash_to_curve_grumpkin(seed, attempt_count);
+        // Safety: We checked that the types match
+        unsafe { *(&point as *const ark_grumpkin::Affine as *const C::Affine) }
+    } else {
+        panic!("Unsupported curve {}", std::any::type_name::<C>())
+    }
+}
+
+fn hash_to_curve_grumpkin(seed: &[u8], attempt_count: u8) -> ark_grumpkin::Affine {
+    let seed_size = seed.len();
+    // expand by 2 bytes to cover incremental hash attempts
+    let mut target_seed = seed.to_vec();
+    target_seed.extend_from_slice(&[0u8; 2]);
+
+    target_seed[seed_size] = attempt_count;
+    target_seed[seed_size + 1] = 0;
+
+    let hash_hi: [u8; blake3::OUT_LEN] = blake3::hash(&target_seed).into();
+    target_seed[seed_size + 1] = 1;
+    let hash_lo: [u8; blake3::OUT_LEN] = blake3::hash(&target_seed).into();
+
+    let mut hash = hash_hi.to_vec();
+    hash.extend_from_slice(&hash_lo);
+
+    // Here we reduce the 512 bit number modulo the base field modulus to calculate `x`
+    let x = ark_grumpkin::Fq::from_be_bytes_mod_order(&hash);
+    let x = ark_grumpkin::Fq::from_base_prime_field(x);
+
+    if let Some(point) = ark_grumpkin::Affine::get_point_from_x_unchecked(x, false) {
+        let parity_bit = hash_hi[0] > 127;
+        let y_bit_set = point.y().unwrap().into_bigint().get_bit(0);
+        if (parity_bit && !y_bit_set) || (!parity_bit && y_bit_set) {
+            -point
+        } else {
+            point
+        }
+    } else {
+        hash_to_curve_grumpkin(seed, attempt_count + 1)
+    }
+}
+
+fn hash_to_curve_bn254(seed: &[u8], attempt_count: u8) -> ark_bn254::G1Affine {
+    let seed_size = seed.len();
+    // expand by 2 bytes to cover incremental hash attempts
+    let mut target_seed = seed.to_vec();
+    target_seed.extend_from_slice(&[0u8; 2]);
+
+    target_seed[seed_size] = attempt_count;
+    target_seed[seed_size + 1] = 0;
+
+    let hash_hi: [u8; blake3::OUT_LEN] = blake3::hash(&target_seed).into();
+    target_seed[seed_size + 1] = 1;
+    let hash_lo: [u8; blake3::OUT_LEN] = blake3::hash(&target_seed).into();
+
+    let mut hash = hash_hi.to_vec();
+    hash.extend_from_slice(&hash_lo);
+
+    // Here we reduce the 512 bit number modulo the base field modulus to calculate `x`
+    let x = ark_bn254::Fq::from_be_bytes_mod_order(&hash);
+    let x = ark_bn254::Fq::from_base_prime_field(x);
+
+    if let Some(point) = ark_bn254::G1Affine::get_point_from_x_unchecked(x, false) {
+        let parity_bit = hash_hi[0] > 127;
+        let y_bit_set = point.y().unwrap().into_bigint().get_bit(0);
+        if (parity_bit && !y_bit_set) || (!parity_bit && y_bit_set) {
+            -point
+        } else {
+            point
+        }
+    } else {
+        hash_to_curve_bn254(seed, attempt_count + 1)
+    }
+}
