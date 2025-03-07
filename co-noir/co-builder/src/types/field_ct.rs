29e8237ad51e828b0340bf290f14e079fb61bc2b
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2025-03-07 13:15:17 +0100

    .

--- co-noir/co-builder/src/types/field_ct.rs
+++ co-noir/co-builder/src/types/field_ct.rs
@@ -1917,6 +1917,7 @@ impl<P: HonkCurve<TranscriptFieldType>, T: NoirWitnessExtensionProtocol<P::Scala
             }
         }
 
+        // We translate the Point LUTs to field luts
         // TACEO TODO this whole process could be batched
         let mut native_straus_tables_x = Vec::with_capacity(native_straus_tables.len());
         let mut native_straus_tables_y = Vec::with_capacity(native_straus_tables.len());
@@ -1948,25 +1949,34 @@ impl<P: HonkCurve<TranscriptFieldType>, T: NoirWitnessExtensionProtocol<P::Scala
                 }
             }
 
-            // TACEO TODO this whole process could be batched (translation and the lut themselves)
             for (
-                scalar_sclice,
-                native_straus_table_x,
-                native_straus_table_y,
-                native_straus_table_i,
+                j,
+                (
+                    scalar_sclice,
+                    native_straus_table_x,
+                    native_straus_table_y,
+                    native_straus_table_i,
+                ),
             ) in izip!(
                 scalar_slices.iter(),
                 native_straus_tables_x.iter(),
                 native_straus_tables_y.iter(),
                 native_straus_tables_i.iter()
-            ) {
-                let index = T::public_zero(); // TODO
+            )
+            .enumerate()
+            {
+                let index = T::public_zero();
+                todo!("Real index");
 
+                // TACEO TODO batch the reads
                 let x = driver.read_lut_by_acvm_type(index.to_owned(), native_straus_table_x)?;
                 let y: <T as NoirWitnessExtensionProtocol<<P as Pairing>::ScalarField>>::AcvmType =
                     driver.read_lut_by_acvm_type(index.to_owned(), native_straus_table_y)?;
                 let i = driver.read_lut_by_acvm_type(index, native_straus_table_i)?;
                 let point = driver.field_shares_to_pointshare::<P::CycleGroup>(x, y, i)?;
+                accumulator = driver.add_points(accumulator, point);
+                operation_transcript.push(accumulator.to_owned());
+                offset_generator_accumulator += offset_generators[j + 1];
 
                 todo!("Implement variable_base_batch_mul_internal")
             }
