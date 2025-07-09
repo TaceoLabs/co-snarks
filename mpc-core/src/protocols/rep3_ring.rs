86c29754c06a5cc714e8a8dbbe473f11fcfbfba2
Daniel Kales <11509575+dkales@users.noreply.github.com>
2025-07-09 13:08:19 +0200

    add leftover method from before rebase

--- mpc-core/src/protocols/rep3_ring.rs
+++ mpc-core/src/protocols/rep3_ring.rs
@@ -73,6 +73,26 @@ where
     [share1, share2, share3]
 }
 
+/// Secret shares a vector of ring elements using replicated secret sharing and the provided random number generator. The ring elements are split into three binary shares each, where each party holds two. The outputs are of type [`Rep3RingShare`].
+pub fn share_ring_elements_binary<T: IntRing2k, R: Rng + CryptoRng>(
+    vals: &[RingElement<T>],
+    rng: &mut R,
+) -> [Vec<Rep3RingShare<T>>; 3]
+where
+    Standard: Distribution<T>,
+{
+    let mut shares1 = Vec::with_capacity(vals.len());
+    let mut shares2 = Vec::with_capacity(vals.len());
+    let mut shares3 = Vec::with_capacity(vals.len());
+    for val in vals {
+        let [share1, share2, share3] = share_ring_element_binary(val.to_owned(), rng);
+        shares1.push(share1);
+        shares2.push(share2);
+        shares3.push(share3);
+    }
+    [shares1, shares2, shares3]
+}
+
 /// Reconstructs a ring element from its arithmetic replicated shares.
 pub fn combine_ring_element<T: IntRing2k>(
     share1: Rep3RingShare<T>,
