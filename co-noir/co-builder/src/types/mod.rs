ac412e54d05d31f473836f7dfb50b36383d37df4
Florin F <156660445+florin5f@users.noreply.github.com>
2025-08-27 14:05:55 +0200

    feat: add builder type transcript

--- co-noir/co-builder/src/types/mod.rs
+++ co-noir/co-builder/src/types/mod.rs
@@ -5,6 +5,7 @@ pub(crate) mod blake3;
 pub(crate) mod blake_util;
 pub(crate) mod field_ct;
 pub(crate) mod generators;
+pub(crate) mod goblin_types;
 pub(crate) mod plookup;
 pub(crate) mod poseidon2;
 pub(crate) mod rom_ram;
