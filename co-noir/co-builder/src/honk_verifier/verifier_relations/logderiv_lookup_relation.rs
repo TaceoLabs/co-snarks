ffb905deb443a05744f34b8b9b641138ebe29e5e
Cesar199999 <cesar.descalzo2@gmail.com>
2025-11-14 14:51:38 +0100

    fix logderiv relation

--- co-noir/co-builder/src/honk_verifier/verifier_relations/logderiv_lookup_relation.rs
+++ co-noir/co-builder/src/honk_verifier/verifier_relations/logderiv_lookup_relation.rs
@@ -38,8 +38,8 @@ impl LogDerivLookupRelation {
         let row_has_write = input.witness.lookup_read_tags().to_owned();
         let row_has_read = input.precomputed.q_lookup().to_owned();
 
-        Ok(row_has_read
-            .multiply(&row_has_write, builder, driver)?
+        Ok(row_has_write
+            .multiply(&row_has_read, builder, driver)?
             .neg()
             .add(&row_has_write, builder, driver)
             .add(&row_has_read, builder, driver))
@@ -55,9 +55,9 @@ impl LogDerivLookupRelation {
         driver: &mut T,
     ) -> HonkProofResult<FieldCT<C::ScalarField>> {
         let gamma = &relation_parameters.gamma;
-        let eta_1 = &relation_parameters.eta_1;
-        let eta_2 = &relation_parameters.eta_2;
-        let eta_3 = &relation_parameters.eta_3;
+        let eta_1 = relation_parameters.eta_1.to_owned();
+        let eta_2 = relation_parameters.eta_2.to_owned();
+        let eta_3 = relation_parameters.eta_3.to_owned();
         let w_1 = input.witness.w_l().to_owned();
         let w_2 = input.witness.w_r().to_owned();
         let w_3 = input.witness.w_o().to_owned();
@@ -72,27 +72,39 @@ impl LogDerivLookupRelation {
         // The wire values for lookup gates are accumulators structured in such a way that the differences w_i -
         // step_size*w_i_shift result in values present in column i of a corresponding table. See the documentation in
         // method get_lookup_accumulators() in  for a detailed explanation.
-        let mut derived_table_entry_1 = negative_column_1_step_size
+        let derived_table_entry_1 = negative_column_1_step_size
             .multiply(&w_1_shift, builder, driver)?
-            .add(&w_1, builder, driver)
-            .add(gamma, builder, driver);
+            .add(&(w_1.add(gamma, builder, driver)), builder, driver);
 
         // (w_1 + \gamma q_2*w_1_shift) + η(w_2 + q_m*w_2_shift) + η₂(w_3 + q_c*w_3_shift) + η₃q_index.
         let derived_table_entry_2 = negative_column_2_step_size
             .multiply(&w_2_shift, builder, driver)?
-            .add(&w_2, builder, driver)
-            .multiply(eta_1, builder, driver)?;
+            .add(&w_2, builder, driver);
+
         let derived_table_entry_3 = negative_column_3_step_size
             .multiply(&w_3_shift, builder, driver)?
-            .add(&w_3, builder, driver)
-            .multiply(eta_2, builder, driver)?;
+            .add(&w_3, builder, driver);
 
-        derived_table_entry_1 = derived_table_entry_1
-            .add(&derived_table_entry_2, builder, driver)
-            .add(&derived_table_entry_3, builder, driver);
+        let [
+            table_index_entry,
+            derived_table_entry_2,
+            derived_table_entry_3,
+        ] = FieldCT::multiply_many(
+            &[table_index, derived_table_entry_2, derived_table_entry_3],
+            &[eta_3, eta_1, eta_2],
+            builder,
+            driver,
+        )?
+        .try_into()
+        .expect("We checked lengths match");
 
-        let table_index = table_index.multiply(eta_3, builder, driver)?;
-        Ok(derived_table_entry_1.add(&table_index, builder, driver))
+        Ok(derived_table_entry_2
+            .add(&derived_table_entry_3, builder, driver)
+            .add(
+                &derived_table_entry_1.add(&table_index_entry, builder, driver),
+                builder,
+                driver,
+            ))
     }
 
     fn compute_write_term_verifier<
@@ -105,33 +117,35 @@ impl LogDerivLookupRelation {
         driver: &mut T,
     ) -> HonkProofResult<FieldCT<C::ScalarField>> {
         let gamma = &relation_parameters.gamma;
-        let eta_1 = &relation_parameters.eta_1;
-        let eta_2 = &relation_parameters.eta_2;
-        let eta_3 = &relation_parameters.eta_3;
+        let eta_1 = relation_parameters.eta_1.to_owned();
+        let eta_2 = relation_parameters.eta_2.to_owned();
+        let eta_3 = relation_parameters.eta_3.to_owned();
 
-        let table_1 = input.precomputed.table_1();
-        let table_2 = input.precomputed.table_2();
-        let table_3 = input.precomputed.table_3();
-        let table_4 = input.precomputed.table_4();
+        let table_1 = input.precomputed.table_1().to_owned();
+        let table_2 = input.precomputed.table_2().to_owned();
+        let table_3 = input.precomputed.table_3().to_owned();
+        let table_4 = input.precomputed.table_4().to_owned();
 
-        Ok(table_1
-            .to_owned()
-            .add(gamma, builder, driver)
+        let mut mul_raw = FieldCT::multiply_many_raw(
+            &[table_2, table_3, table_4],
+            &[eta_1, eta_2, eta_3],
+            builder,
+            driver,
+        )?;
+
+        Ok(FieldCT::commit_mul(&mut mul_raw[0], builder)?
             .add(
-                &table_2.to_owned().multiply(eta_1, builder, driver)?,
+                &FieldCT::commit_mul(&mut mul_raw[1], builder)?,
                 builder,
                 driver,
             )
             .add(
-                &table_3.to_owned().multiply(eta_2, builder, driver)?,
+                &FieldCT::commit_mul(&mut mul_raw[2], builder)?,
                 builder,
                 driver,
             )
-            .add(
-                &table_4.to_owned().multiply(eta_3, builder, driver)?,
-                builder,
-                driver,
-            ))
+            .add(&table_1, builder, driver)
+            .add(gamma, builder, driver))
     }
 }
 
@@ -157,27 +171,21 @@ impl<C: HonkCurve<TranscriptFieldType>> Relation<C> for LogDerivLookupRelation {
         let write_term =
             Self::compute_write_term_verifier(input, relation_parameters, builder, driver)?;
 
-        let [write_inverse, read_inverse] = FieldCT::multiply_many(
-            &[read_term, write_term.clone()],
-            &[inverses.clone(), inverses.clone()],
-            builder,
-            driver,
-        )?
-        .try_into()
-        .expect("We checked lengths match");
-
         // Establish the correctness of the polynomial of inverses I. Note: inverses is computed so that the value is 0
         // if !inverse_exists.
         // Degrees:                     2 (3)       1 (2)        1              1
-        let tmp = write_term
-            .multiply(&write_inverse, builder, driver)?
+        let logderiv_first_term = read_term
+            .multiply(&write_term, builder, driver)?
+            .multiply(&inverses, builder, driver)?
             .sub(&inverse_exists, builder, driver)
             .multiply(scaling_factor, builder, driver)?;
-        accumulator.r0 = accumulator.r0.add(&tmp, builder, driver);
+        accumulator
+            .r0
+            .add_assign(&logderiv_first_term, builder, driver);
 
-        let mul = FieldCT::multiply_many(
-            &[write_inverse, read_tag.clone()],
-            &[read_counts.clone(), read_tag.clone()],
+        let mut mul_raw = FieldCT::multiply_many_raw(
+            &[read_selector, read_counts, read_tag.clone()],
+            &[write_term, read_term, read_tag.clone()],
             builder,
             driver,
         )?;
@@ -186,17 +194,19 @@ impl<C: HonkCurve<TranscriptFieldType>> Relation<C> for LogDerivLookupRelation {
         // i.e. enforced across the entire trace, not on a per-row basis.
         // Degrees:                       1            2 (3)            1            3 (4)
         //
-        let tmp = read_selector
-            .multiply(&read_inverse, builder, driver)?
-            .sub(&mul[0], builder, driver);
-        accumulator.r1 = accumulator.r1.add(&tmp, builder, driver);
-
+        let tmp = FieldCT::commit_mul(&mut mul_raw[0], builder)?
+            .sub(
+                &FieldCT::commit_mul(&mut mul_raw[1], builder)?,
+                builder,
+                driver,
+            )
+            .multiply(&inverses, builder, driver)?;
+        accumulator.r1.add_assign(&tmp, builder, driver);
         // we should make sure that the read_tag is a boolean value
-        let tmp =
-            mul[1]
-                .sub(&read_tag, builder, driver)
-                .multiply(scaling_factor, builder, driver)?;
-        accumulator.r2 = accumulator.r2.add(&tmp, builder, driver);
+        let tmp = FieldCT::commit_mul(&mut mul_raw[2], builder)?
+            .sub(&read_tag, builder, driver)
+            .multiply(scaling_factor, builder, driver)?;
+        accumulator.r2.add_assign(&tmp, builder, driver);
         Ok(())
     }
 }
