681b3edd1d92303d4a4461afc5b33b803b4fd070
Cesar199999 <cesar.descalzo2@gmail.com>
2026-05-08 11:39:35 +0200

    clippy

--- co-noir/co-builder/tests/batch_mul.rs
+++ co-noir/co-builder/tests/batch_mul.rs
@@ -212,12 +212,12 @@ fn test_batch_mul_short_non_trivial_scalars_plaindriver() {
 
         expected_result = (expected_result + point * scalar).into_affine();
         points.push(BigGroup::new(
-            BigField::from_witness_other_acvm_type(&point.x.into(), &mut driver, &mut builder)
+            BigField::from_witness_other_acvm_type(&point.x, &mut driver, &mut builder)
                 .unwrap(),
-            BigField::from_witness_other_acvm_type(&point.y.into(), &mut driver, &mut builder)
+            BigField::from_witness_other_acvm_type(&point.y, &mut driver, &mut builder)
                 .unwrap(),
         ));
-        scalars.push(FieldCT::from_witness(scalar.into(), &mut builder));
+        scalars.push(FieldCT::from_witness(scalar, &mut builder));
     }
 
     run_test(
